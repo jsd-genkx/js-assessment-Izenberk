8fce47c818e162b05cc95e9107e191d773ae2d24
Izenberk <aceskorn555@outlook.com>
2025-07-21 11:54:05 +0700

    Morning Checkpoint + feat: level selection

--- main.js
+++ main.js
@@ -84,6 +84,79 @@ class Field {
 		this.field[this.positionRow][this.positionCol] = "*";
 	}
 
+	// Map generation
+	static generateField(height, width, holePercentage) {
+		const field = [];
+
+		for (let row = 0; row < height; row++) {
+			const rowArr = [];
+			for (let col = 0; col < width; col++) {
+				const isHole = Math.random() < holePercentage;
+				rowArr.push(isHole? 'O' : '░');
+			}
+			field.push(rowArr);
+		}
+		// Set player at [0][0]
+		field[0][0] = '*';
+
+		// Set hat position
+		let hatRow, hatCol;
+
+		do {
+			hatRow = Math.floor(Math.random() * height);
+			hatCol = Math.floor(Math.random() * width);
+		} while (hatRow === 0 && hatCol === 0);
+
+		field[hatRow][hatCol] = '^';
+
+		return field;
+	}
+
+	static menu() {
+		console.log("=== FIND YOUR HAT ===");
+		const difficulty = prompt("Choose difficulty: \n1. Easy\n2. Medium\n3. Hard\n> ").trim();
+		const mapType = promp("Use Pre-made map (P) or Random map (R)?").toUpperCase();
+	
+		let field;
+		let jumps = 2;
+		let useEnemy = false;
+		let size = 4;
+
+		// Difficulty settings
+		switch (difficulty) {
+			case '1':
+				jumps = 2;
+				size = 4;
+				break;
+			case '2':
+				jumps = 2;
+				size = 5;
+				break;
+			case '3':
+				jumps = 1;
+				size = 6;
+				useEnemy = true;
+				break;
+			default:
+				console.log("Invalid input. Defaulting to Easy.");
+				jumps = 2;
+				size = 4;
+		}
+
+		// Map selection
+		if (mapType === 'P') {
+			field = Field.getPreMadeMap(size);
+		} else if (mapType === 'R') {
+			const holePercentage = 0.3;
+			field.generateField(size, size, holePercentage);
+		} else {
+			console.log("Invalid input. Using pre-made by default.");
+			field = Field.getPreMadeMap(size);
+		}
+
+
+	}
+
 	startGame() {
 		this.print();
 		console.log("Use W, A, S, D to move or X to exit game");
